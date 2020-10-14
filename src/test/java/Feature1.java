67dc4e53a53dbf16c448d7625ca08b3339ee9dfb
Abbyzt07 <brhnbyzt@hotmail.com>
2020-10-14 17:46:05 +0200

    Awesome feature 1 more added

--- src/test/java/Feature1.java
+++ src/test/java/Feature1.java
@@ -1,5 +1,6 @@
 public class Feature1 {
     public static void main(String[] args) {
         System.out.println("Feature 1 is being developed");
+        System.out.println("Feature 1 more changes");
     }
 }
