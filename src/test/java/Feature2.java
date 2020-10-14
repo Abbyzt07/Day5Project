547dabd26810e592b85fa4895c36ecd77d11f600
Abbyzt07 <brhnbyzt@hotmail.com>
2020-10-14 17:43:05 +0200

    Awesome feature 2 more history

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -1,5 +1,7 @@
 public class Feature2 {
     public static void main(String[] args) {
         System.out.println("Working in feature 2");
+        System.out.println("Working in feature 2 more");
+        System.out.println("Working in feature 2 again");
     }
 }
