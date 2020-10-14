b7aabdaa51c230bac101c8dc1ae8ae0c9d33a413
Abbyzt07 <72063503+Abbyzt07@users.noreply.github.com>
2020-10-14 18:06:03 +0200

    added changes from GitHub

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Really awesome hello");
+        System.out.println("Really from GITHUB MADE SOME CHANGES awesome hello");
     }
 }
